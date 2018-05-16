ad1e7da99d81486a7c29e4e285f1534812282716
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-16 13:26:48 +0100

    remove pycall dependencie

---
+++ deps/build.jl
@@ -0,0 +1,17 @@
+const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
+
+!isdir(_pkg_assets) && mkdir(_pkg_assets)
+
+deps = [
+    "https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz",
+    "https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic",
+    "https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic",
+    "http://gnab.github.io/remark/downloads/remark-latest.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML&delayStartupUntil=configured"
+]
+
+depnames = joinpath.(_pkg_assets, ["font1.css", "font2.css", "font3.css", "remark.min.js", "mathjax.js"])
+
+for (dep, depname) in zip(deps, depnames)
+    download(dep, depname)
+end
--- src/Remark.jl
+++ src/Remark.jl
@@ -1,37 +1,53 @@
 module Remark
 
-using PyCall
 import Literate: filename
 
-@pyimport webbrowser
-
 const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
+
+const online_deps = Dict(
+    "\$font1" => "url(https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz)",
+    "\$font2" => "url(https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic)",
+    "\$font3" => "url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic)",
+    "\$remark" => "http://gnab.github.io/remark/downloads/remark-latest.min.js",
+    "\$mathjax" => "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML&delayStartupUntil=configured"
+)
 
 function slideshow(inputfile, outputdir =""; name = filename(inputfile))
     slideshowdir = joinpath(outputdir, name)
     mkpath(slideshowdir)
-    _create_index(slideshowdir)
+    _create_index_html(slideshowdir)
     cp(inputfile, joinpath(slideshowdir, "markdown.md"), remove_destination=true)
     return slideshowdir
 end
 
-function _create_index(slideshowdir)
-    d = Dict("\$font1" => "url(https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz)",
-             "\$font2" => "url(https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic)",
-             "\$font3" => "url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic)",
-             "\$remark" => "http://gnab.github.io/remark/downloads/remark-latest.min.js",
-             "\$ajax" => "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML&delayStartupUntil=configured")
-    s = readstring(joinpath(_pkg_assets, "indextemplate.html"))
-    for (key, val) in d
-        s = replace(s, key, val)
-    end
+function _create_index_html(slideshowdir)
+
+    d = online_deps
+
     Base.open(joinpath(slideshowdir, "index.html"), "w") do f
-      write(f, s)
+        template = Base.open(joinpath(_pkg_assets, "indextemplate.html"))
+        for line in eachline(template, chomp=false)
+            for (key, val) in d
+                line = replace(line, key, val)
+            end
+            write(f, line)
+        end
+        close(template)
+    end
+end
+
+function openurl(url::AbstractString)
+    if is_apple()
+        run(`open $url`)
+    elseif is_windows()
+        run(`start $url`)
+    elseif is_unix()
+        run(`xdg-open $url`)
     end
 end
 
 function open(slideshowdir)
-    webbrowser.open(joinpath(slideshowdir, "index.html"))
+    openurl(joinpath(slideshowdir, "index.html"))
 end
 
 end # module
