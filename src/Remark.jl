36679d662610f96884f19d9dc034429790dcf2a7
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-16 12:31:37 +0100

    preliminary local support

--- src/Remark.jl
+++ src/Remark.jl
@@ -10,9 +10,24 @@ const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
 function slideshow(inputfile, outputdir =""; name = filename(inputfile))
     slideshowdir = joinpath(outputdir, name)
     mkpath(slideshowdir)
-    cp(joinpath(_pkg_assets, "index.html"), joinpath(slideshowdir, "index.html"), remove_destination=true)
+    _create_index(slideshowdir)
     cp(inputfile, joinpath(slideshowdir, "markdown.md"), remove_destination=true)
     return slideshowdir
+end
+
+function _create_index(slideshowdir)
+    d = Dict("\$font1" => "url(https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz)",
+             "\$font2" => "url(https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic)",
+             "\$font3" => "url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic)",
+             "\$remark" => "http://gnab.github.io/remark/downloads/remark-latest.min.js",
+             "\$ajax" => "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML&delayStartupUntil=configured")
+    s = readstring(joinpath(_pkg_assets, "indextemplate.html"))
+    for (key, val) in d
+        s = replace(s, key, val)
+    end
+    Base.open(joinpath(slideshowdir, "index.html"), "w") do f
+      write(f, s)
+    end
 end
 
 function open(slideshowdir)
