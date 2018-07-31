92670534c7b451c38b3e909386a6e5834d46cd0a
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-31 16:53:18 +0100

    allow to bundle libraries in project

--- src/Remark.jl
+++ src/Remark.jl
@@ -17,24 +17,31 @@ const deps = [
     "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css"
 ]
 
-const depfiles = joinpath.(_pkg_assets, ["font1.css", "font2.css", "font3.css", "remark.min.js", "katex.min.js", "auto-render.min.js", "katex.min.css"])
+const depnames =  ["font1.css", "font2.css", "font3.css", "remark.min.js", "katex.min.js", "auto-render.min.js", "katex.min.css"]
+const depfiles = joinpath.(_pkg_assets, depnames)
+const libdepfiles = joinpath.("..", "lib", depnames)
 
 const depkeys = ["\$font1", "\$font2", "\$font3", "\$remark", "\$katexjs", "\$auto-render", "\$katexcss"]
 
 function slideshow(inputfile, outputdir; js = :local, documenter = true)
     inputfile = realpath(abspath(inputfile))
     outputdir = realpath(abspath(outputdir))
-    mkpath.(joinpath.(outputdir, ("src", "build")))
-    _create_index_md(inputfile, outputdir; documenter = documenter)
+    mkpath.(joinpath.(outputdir, ("src", "build", "lib")))
+    _create_index_md(inputfile, outputdir; js = js, documenter = documenter)
     _create_index_html(outputdir; js = js)
     return outputdir
 end
 
-function _create_index_md(inputfile, outputdir; documenter = true)
+function _create_index_md(inputfile, outputdir; js = :local, documenter = true)
     if ismatch(r".jl$", inputfile)
         Literate.markdown(inputfile, joinpath(outputdir, "src"), name = "index")
     else
         cp(inputfile, joinpath(outputdir, "src", "index.md"), remove_destination=true)
+    end
+    if js == :lib
+        for (name, file) in zip(depnames, depfiles)
+            cp(file, joinpath(outputdir, "lib", name))
+        end
     end
     srand(123)
     s = randstring(50)
@@ -51,7 +58,8 @@ end
 
 function _create_index_html(outputdir; js = :local)
 
-    d = (js == :local) ? depfiles : deps
+    d = (js == :local) ? depfiles :
+        (js == :lib) ? libdepfiles : deps
 
     Base.open(joinpath(outputdir, "build", "index.html"), "w") do f
         template = Base.open(joinpath(_pkg_assets, "indextemplate.html"))
