35bf773978431481d7d8bb4ea63aa07ea8ae2a07
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-28 18:02:47 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Remark
 using Test
 
-demo = realpath(abspath(joinpath(@__DIR__, "..", "demo")))
+demo = joinpath(@__DIR__, "..", "demo")
 isdir(demo) && rm(demo, recursive = true)
 sleep(1)
 
@@ -16,12 +16,24 @@ end
 
 @testset "slideshow" begin
     slideshowdir = Remark.slideshow(joinpath(demo, "example.jl"))
-    @test slideshowdir == demo
+    @test slideshowdir == realpath(abspath(demo))
     @test all(isdir, joinpath.(slideshowdir, "build", "fonts", ["Lora", "Ubuntu_Mono", "Yanone_Kaffeesatz"]))
     for file in Remark.depfiles
         @test isfile(joinpath(slideshowdir, "build", splitdir(file)[2]))
     end
     v1 = readlines(joinpath(@__DIR__, "index.html"))
+    v2 = readlines(joinpath(demo, "build", "index.html"))
+    @test all(v1 .== v2)
+end
+
+@testset "slideshowmd" begin
+    slideshowdir = Remark.slideshow(joinpath(demo, "example.md"))
+    @test slideshowdir == realpath(abspath(demo))
+    @test all(isdir, joinpath.(slideshowdir, "build", "fonts", ["Lora", "Ubuntu_Mono", "Yanone_Kaffeesatz"]))
+    for file in Remark.depfiles
+        @test isfile(joinpath(slideshowdir, "build", splitdir(file)[2]))
+    end
+    v1 = readlines(joinpath(@__DIR__, "indexmd.html"))
     v2 = readlines(joinpath(demo, "build", "index.html"))
     @test all(v1 .== v2)
 end
