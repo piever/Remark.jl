02bcc9d0c3318b2d42115bce352263c288280019
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-28 17:38:34 +0100

    test deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,24 @@
 using Remark
 using Test
 
-rm(joinpath(@__DIR__, "..", "demo"), force = true, recursive = true)
+demo = realpath(abspath(joinpath(@__DIR__, "..", "demo")))
+isdir(demo) && rm(demo, recursive = true)
 sleep(1)
 
-mkdir(joinpath(@__DIR__, "..", "demo"))
+mkdir(demo)
 filenames = ["example.jl", "example.md"]
 cp.(joinpath.(@__DIR__, "..", "examples", filenames), joinpath.(@__DIR__, "..", "demo", filenames), force = true)
 sleep(1)
-# write your own tests here
+
 @testset "deps" begin
     @test all(isfile, Remark.depfiles)
 end
 
 @testset "slideshow" begin
-    slideshowdir = Remark.slideshow(joinpath(@__DIR__, "..", "demo", "example.jl"))
-    @test slideshowdir == realpath(abspath(joinpath(@__DIR__, "..", "demo")))
+    slideshowdir = Remark.slideshow(joinpath(demo, "example.jl"))
+    @test slideshowdir == demo
     @test all(isdir, joinpath.(slideshowdir, "build", "fonts", ["Lora", "Ubuntu_Mono", "Yanone_Kaffeesatz"]))
+    for file in Remark.depfiles
+        @test isfile(joinpath(slideshowdir, "build", splitdir(file)[2]))
+    end
 end
