3765357a1f3bc903a529f8c7b70b8d203d0dff6c
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-28 17:55:17 +0100

    Add tests (#13)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,29 @@
 using Remark
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
+using Test
+
+demo = realpath(abspath(joinpath(@__DIR__, "..", "demo")))
+isdir(demo) && rm(demo, recursive = true)
+sleep(1)
+
+mkdir(demo)
+filenames = ["example.jl", "example.md"]
+cp.(joinpath.(@__DIR__, "..", "examples", filenames), joinpath.(@__DIR__, "..", "demo", filenames), force = true)
+sleep(1)
+
+@testset "deps" begin
+    @test all(isfile, Remark.depfiles)
 end
 
-# write your own tests here
-@test 1 == 2
+@testset "slideshow" begin
+    slideshowdir = Remark.slideshow(joinpath(demo, "example.jl"))
+    @test slideshowdir == demo
+    @test all(isdir, joinpath.(slideshowdir, "build", "fonts", ["Lora", "Ubuntu_Mono", "Yanone_Kaffeesatz"]))
+    for file in Remark.depfiles
+        @test isfile(joinpath(slideshowdir, "build", splitdir(file)[2]))
+    end
+    v1 = readlines(joinpath(@__DIR__, "index.html"))
+    v2 = readlines(joinpath(demo, "build", "index.html"))
+    @test all(v1 .== v2)
+end
+
+rm(demo, recursive = true)
