e740d43b32a751f83b9f41f93552d30d4ee64497
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-12-31 13:38:50 +0100

    Add KaTeX fonts (#19)
    * download katex fonts
    
    * update gitignore
    
    * added katexfont test
    
    * use release tarball
    
    * remove extra file
    
    * fix tests

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,30 @@
+import BinaryProvider
+
 const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
-deps = [
+download(
     "http://gnab.github.io/remark/downloads/remark-latest.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/contrib/auto-render.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
-]
+    joinpath(_pkg_assets, "remark.min.js")
+)
 
-depnames = joinpath.(_pkg_assets, ["remark.min.js", "katex.min.js", "auto-render.min.js", "katex.min.css"])
+const tmp = joinpath(_pkg_assets, "tmp")
+!isdir(tmp) && mkdir(tmp)
 
-for (dep, depname) in zip(deps, depnames)
-    download(dep, depname)
+download("https://github.com/KaTeX/KaTeX/releases/download/v0.10.0/katex.tar.gz", joinpath(tmp, "katex.tar.gz"))
+
+BinaryProvider.gen_unpack_cmd(joinpath(tmp, "katex.tar.gz"), tmp) |> run
+
+const katexdir = joinpath(tmp, "katex")
+const katexfontdir = joinpath(katexdir, "fonts")
+
+for font in readdir(katexfontdir)
+    mv(joinpath(katexfontdir, font), joinpath(_pkg_assets, "fonts", font), force = true)
 end
+
+for file in ["katex.min.js", "katex.min.css", joinpath("contrib", "auto-render.min.js")]
+    mv(joinpath(katexdir, file), joinpath(_pkg_assets, splitdir(file)[2]), force = true)
+end
+
+rm(tmp, recursive=true)
