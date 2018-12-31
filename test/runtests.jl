dc6ebd3d82428a86f95faf80330149b1e087b156
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-12-31 11:32:53 +0000

    added katexfont test

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,41 @@ sleep(1)
     @test all(isfile, Remark.depfiles)
 end
 
+
+const katexfonts = [
+    "KaTeX_AMS-Regular",
+    "KaTeX_Caligraphic-Bold",
+    "KaTeX_Caligraphic-Regular",
+    "KaTeX_Fraktur-Bold",
+    "KaTeX_Fraktur-Regular",
+    "KaTeX_Main-Regular",
+    "KaTeX_Main-Bold",
+    "KaTeX_Main-Italic",
+    "KaTeX_Main-BoldItalic",
+    "KaTeX_Math-Regular",
+    "KaTeX_Math-Italic",
+    "KaTeX_Math-BoldItalic",
+    "KaTeX_SansSerif-Bold",
+    "KaTeX_SansSerif-Italic",
+    "KaTeX_SansSerif-Regular",
+    "KaTeX_Script-Regular",
+    "KaTeX_Size1-Regular",
+    "KaTeX_Size2-Regular",
+    "KaTeX_Size3-Regular",
+    "KaTeX_Size4-Regular",
+    "KaTeX_Typewriter-Regular",
+]
+
+const exts =["woff", "woff2"]
+
+@testset "katexfonts" begin
+    for font in katexfonts
+        for ext in exts
+            @test isfile(joinpath(@__DIR__, "..", "assets", "fonts", join([font, ext], '.')))
+        end
+    end
+end
+
 isalnum(x) = isnumeric(x) || isletter(x)
 
 @testset "slideshow" begin
