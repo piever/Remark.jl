954e4f5033fd436f7dc05e226be2ffde9b9b503b
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2020-07-01 22:08:29 +0100

    string manipulation cleanup (#40)
    * some string cleanup
    
    * rename
    
    * typo

--- src/Remark.jl
+++ src/Remark.jl
@@ -47,13 +47,13 @@ function _create_index_md(inputfile, outputdir; documenter = true)
     s2 = randstring('a':'z', 50)
     outputfile = joinpath(outputdir, "build", "index.md")
     if documenter
-        _replace_line(
+        replace_linewise(
             joinpath(outputdir, "src", "index.md"),
             r"^(\s)*(--)(\s)*$" => s1,
             r"^(\s)*(\$\$)(\s)*$" => s2,
         )
         Documenter.makedocs(format = DocumenterMarkdown.Markdown(), root = outputdir)
-        _replace_line(
+        replace_linewise(
             outputfile,
             Regex("^($s1)\$") => "--",
             s2 => raw"$$",
@@ -65,26 +65,20 @@ function _create_index_md(inputfile, outputdir; documenter = true)
     outputfile
 end
 
-
 function _create_index_html(outputdir, md_file, options = Dict(); title = "Title")
+    
+    optionsjs = JSON.json(options)
+    template = joinpath(_pkg_assets, "indextemplate.html")
+    replacements = ["\$title" => title, "\$options" => optionsjs]
 
-    Base.open(joinpath(outputdir, "build", "index.html"), "w") do f
-        template = Base.open(joinpath(_pkg_assets, "indextemplate.html"))
+    Base.open(joinpath(outputdir, "build", "index.html"), "w") do io
         for line in eachline(template, keep=true)
-            if occursin(r"\$title", line)
-                title_line = replace(line, r"\$title" => title)
-                write(f, title_line)
-            elseif occursin(r"\$presentation", line)
-                copytobuffer!(f, md_file) 
-            elseif occursin(r"\$options", line)
-                optionsjs = JSON.json(options)
-                option_line = replace(line, r"\$options" => optionsjs)
-                write(f, option_line)
+            if occursin("\$presentation", line)
+                Base.open(md -> write(io, md), md_file)
             else
-                write(f, line)
+                write(io, foldl(replace, replacements, init=line))
             end
         end
-        close(template)
     end
     for (name, file) in zip(depnames, depfiles)
         dest = joinpath(outputdir, "build", name)
@@ -98,13 +92,7 @@ end
 open(outputdir) =
     DefaultApplication.open(joinpath(outputdir, "build", "index.html"))
 
-function copytobuffer!(f, filename)
-    for line in eachline(filename, keep=true)
-        write(f, line)
-    end
-end
-
-function _replace_line(filename, pairs...)
+function replace_linewise(filename, pairs...)
     f = Base.open(filename)
     (tmp, tmpstream) = mktemp()
     for line in eachline(f, keep=false)
@@ -116,6 +104,5 @@ function _replace_line(filename, pairs...)
     close(tmpstream)
     mv(tmp, filename, force=true)
 end
-
 
 end # module
