7d83f4eef1f2cf27a1dfdb1770a2285dfd15877f
piever <pietro.vertechi@protonmail.com>
2023-04-03 12:47:29 +0200

    clean up building step

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-import Tar, CodecZlib
+import Tar, CodecZlib, Downloads
 
 const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
 
@@ -9,30 +9,24 @@ download(
     joinpath(_pkg_assets, "remark.min.js")
 )
 
-const tmp = joinpath(_pkg_assets, "tmp")
-!isdir(tmp) && mkdir(tmp)
+mktempdir() do tmp
+    katex_compressed=joinpath(tmp, "katex.tar.gz")
 
-const download_dir = joinpath(_pkg_assets, "download")
-!isdir(download_dir) && mkdir(download_dir)
+    Downloads.download("https://github.com/KaTeX/KaTeX/releases/download/v0.10.0/katex.tar.gz", katex_compressed)
 
-katex_compressed=joinpath(download_dir, "katex.tar.gz")
+    extract_dir = mkpath(joinpath(tmp, "extract"))
+    open(CodecZlib.GzipDecompressorStream, katex_compressed) do io
+        Tar.extract(io, extract_dir)
+     end
 
-download("https://github.com/KaTeX/KaTeX/releases/download/v0.10.0/katex.tar.gz", katex_compressed)
+    katexdir = joinpath(extract_dir, "katex")
+    katexfontdir = joinpath(katexdir, "fonts")
 
-open(CodecZlib.GzipDecompressorStream, katex_compressed) do io
-   Tar.extract(io, tmp)
+    for font in readdir(katexfontdir)
+        mv(joinpath(katexfontdir, font), joinpath(_pkg_assets, "fonts", font), force = true)
+    end
+
+    for file in ["katex.min.js", "katex.min.css", joinpath("contrib", "auto-render.min.js")]
+        mv(joinpath(katexdir, file), joinpath(_pkg_assets, splitdir(file)[2]), force = true)
+    end
 end
-
-const katexdir = joinpath(tmp, "katex")
-const katexfontdir = joinpath(katexdir, "fonts")
-
-for font in readdir(katexfontdir)
-    mv(joinpath(katexfontdir, font), joinpath(_pkg_assets, "fonts", font), force = true)
-end
-
-for file in ["katex.min.js", "katex.min.css", joinpath("contrib", "auto-render.min.js")]
-    mv(joinpath(katexdir, file), joinpath(_pkg_assets, splitdir(file)[2]), force = true)
-end
-
-rm(tmp, recursive=true)
-rm(download_dir, recursive=true)
