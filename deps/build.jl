9885bd39c44f64ea6e673d49587abba4305b2bad
Pietro Vertechi <pietro.vertechi@protonmail.com>
2023-04-03 13:08:13 +0200

    Removing BuildProvider dependency (#55)
    * Removing BuildProvider dependency given it does not work in M1
    
    * clean up building step
    
    * lower bound julia
    
    * update example files
    
    ---------
    
    Co-authored-by: Sinead Kanlioglu <sk@Sineads-MacBook-Pro.local>

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-import BinaryProvider
+import Tar, CodecZlib, Downloads
 
 const _pkg_assets = joinpath(dirname(@__DIR__), "assets")
 
@@ -9,22 +9,24 @@ download(
     joinpath(_pkg_assets, "remark.min.js")
 )
 
-const tmp = joinpath(_pkg_assets, "tmp")
-!isdir(tmp) && mkdir(tmp)
+mktempdir() do tmp
+    katex_compressed=joinpath(tmp, "katex.tar.gz")
 
-download("https://github.com/KaTeX/KaTeX/releases/download/v0.10.0/katex.tar.gz", joinpath(tmp, "katex.tar.gz"))
+    Downloads.download("https://github.com/KaTeX/KaTeX/releases/download/v0.10.0/katex.tar.gz", katex_compressed)
 
-BinaryProvider.gen_unpack_cmd(joinpath(tmp, "katex.tar.gz"), tmp) |> run
+    extract_dir = mkpath(joinpath(tmp, "extract"))
+    open(CodecZlib.GzipDecompressorStream, katex_compressed) do io
+        Tar.extract(io, extract_dir)
+     end
 
-const katexdir = joinpath(tmp, "katex")
-const katexfontdir = joinpath(katexdir, "fonts")
+    katexdir = joinpath(extract_dir, "katex")
+    katexfontdir = joinpath(katexdir, "fonts")
 
-for font in readdir(katexfontdir)
-    mv(joinpath(katexfontdir, font), joinpath(_pkg_assets, "fonts", font), force = true)
+    for font in readdir(katexfontdir)
+        mv(joinpath(katexfontdir, font), joinpath(_pkg_assets, "fonts", font), force = true)
+    end
+
+    for file in ["katex.min.js", "katex.min.css", joinpath("contrib", "auto-render.min.js")]
+        mv(joinpath(katexdir, file), joinpath(_pkg_assets, splitdir(file)[2]), force = true)
+    end
 end
-
-for file in ["katex.min.js", "katex.min.css", joinpath("contrib", "auto-render.min.js")]
-    mv(joinpath(katexdir, file), joinpath(_pkg_assets, splitdir(file)[2]), force = true)
-end
-
-rm(tmp, recursive=true)
